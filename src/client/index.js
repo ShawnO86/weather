f105d0e16674a2d47d57ac32b2b11129b8c88402
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-01-23 16:34:05 -0600

    work on layout

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,14 @@
 import { getData } from './js/getData'
+import './styles/main.scss'
 
 async function geoData() {
-const data = await getData('http://localhost:3001/geoData/rockford');
-console.log(`From express server -- Long: ${data.long}, Lat: ${data.lat}, Country: ${data.country}, Local Area: ${data.name}, ${data.local}`)
+let city = document.getElementById('cityName').value;
+if(!city) {
+    alert('Must enter a destination.')
+} else {
+    const data = await getData(`http://localhost:3001/geoData/${city}`);
+    console.log(`From express server -- Long: ${data.long}, Lat: ${data.lat}, Country: ${data.country}, Local Area: ${data.name}, ${data.local}`)
+}
 } 
 
 document.getElementById('sub').addEventListener("click", (e) => {
